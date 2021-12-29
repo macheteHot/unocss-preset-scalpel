9964ff37a925bd5eee56db2a1b7349aafabf4f2e
Machete <rtpppp163@gmail.com>
2021-12-30 00:29:41 +0800

    fix types error

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,44 +3,35 @@ import type { UNIT_ENUM } from '../constant'
 export type IDirection = 't' | 'r' | 'b' | 'l' | 'x' | 'y'
 
 export interface IScalpelOptions {
-  /**
-   * @default { red: '#f00', white: '#fff', black: '#000', blue: '#00f', yellow: '#ff0', transparent: 'transparent'}
-   */
   colors?: Record<string, string>
   /**
-   * @default 'px'
+   * @defaultValue 'px'
    */
   unit?: typeof UNIT_ENUM[number]
-  /**
-   * @default { sm: 'media (min-width: 640px)', md: 'media (min-width: 768px)', lg: 'media (min-width: 1024px)', xl: 'media (min-width: 1280px)', }
-   */
   mediaQueries?: Record<string, string>
   /**
-   * @default false
+   * @defaultValue false
    */
   important?: boolean
   /**
-   * @default []
+   * @defaultValue []
    */
   ignoreRules?: string[]
-  /**
-   * @default { unit: 'rem', rootValue: 16, unitPrecision: 5, minPixelValue: 1, }
-   */
   vToAny?: {
     /**
-     * @default 'rem'
+     * @defaultValue 'rem'
      */
     unit: typeof UNIT_ENUM[number]
     /**
-     * @default 16
+     * @defaultValue 16
      */
     rootValue: number
     /**
-     * @default 5
+     * @defaultValue 5
      */
     unitPrecision?: number
     /**
-     * @default 1
+     * @defaultValue 1
      */
     minPixelValue?: number
   }
