aa62f21c1d7dc08084d9d5c323362e46ccfe638c
macheteHot <rtpppp163@gmail.com>
2021-12-29 11:40:47 +0800

    fix test config

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(() => ({
         presetAttributify(),
         presetScalpel({
           important: false,
-          ignoreRules: ['flexBasis'],
+          ignoreRules: [],
         }),
       ],
     }),
