6e90f1e7eac8abcdf14b2959e8caaf0d20e81097
huge <machete@xudeMacBook-Pro.local>
2022-08-21 02:47:11 +0800

    fix: test file

--- tests/rules/marginAndPadding.js
+++ tests/rules/marginAndPadding.js
@@ -2,6 +2,8 @@ import { NumAndUnitList } from '../constant.js'
 
 export default ['m', 'margin', 'p', 'padding'].map((mp) =>
   ['t-', 'r-', 'b-', 'l-', 'x-', 'y-', ''].map((trblxy) =>
-    ['m-', ''].map((m) => NumAndUnitList.map((u) => `${mp}-${trblxy}${m}${u}`))
+    ['m-', ''].map((m) =>
+      [...NumAndUnitList, 'auto'].map((u) => `${mp}-${trblxy}${m}${u}`)
+    )
   )
 )
